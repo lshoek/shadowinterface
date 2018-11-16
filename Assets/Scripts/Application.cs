32943afa40834b9df28589a6c432c708da615846
lshoek <lesleyvanhoek@hotmail.com>
2018-11-16 22:22:59 +0100

    simple 2.5d point gravity with some planetoids and nice lighting

---
+++ Assets/Scripts/Application.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Application : MonoBehaviour
+{
+    public static Application Instance { get; private set; }
+
+    public PointGravity GravityBody;
+
+    public List<Planetoid> planetoids;
+
+    float lastSpawned = 0.0f;
+    float spawnDelay = 0.75f;
+
+    void Start ()
+    {
+        if (Instance != null)
+            Instance = this;
+
+        planetoids = new List<Planetoid>();
+    }
+	
+	void Update ()
+    {
+        if (GravityBody != null)
+        {
+            float elapsedTime = Time.time;
+            Vector3 spawnVector = GravityBody.Position + new Vector3(Mathf.Cos(elapsedTime), 0.0f, Mathf.Sin(elapsedTime)) * 10.0f;
+
+            if (elapsedTime - lastSpawned > spawnDelay)
+            {
+                SpawnPlanetoid(spawnVector);
+                lastSpawned = elapsedTime;
+            }
+            Debug.DrawLine(GravityBody.Position, spawnVector);
+        }
+    }
+
+    #region "Planetoid Wrapper Methods"
+    void SpawnPlanetoid(Vector3 position)
+    {
+        GameObject ob = Instantiate(Resources.Load("Prefabs/Planetoid") as GameObject);
+        ob.name = string.Format("Planetoid{0}", planetoids.Count);
+        ob.transform.position = position;
+
+        // given the prefab this should never fail
+        Planetoid poid = ob.GetComponent<Planetoid>();
+        planetoids.Add(poid);
+        GravityBody.RefreshColliders();
+    }
+
+    void DespawnPlanetoid(Planetoid poid)
+    {
+        planetoids.Remove(poid);
+        Destroy(poid.gameObject);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PointGravity.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PointGravity : MonoBehaviour
+{
+    public float range = 10f;
+    Rigidbody mainRb;
+
+    public Collider[] Colliders { get; private set; }
+
+    public Vector3 Position { get { return mainRb.position; } }
+
+    void Start()
+    {
+        mainRb = GetComponent<Rigidbody>();
+        RefreshColliders();
+    }
+
+    void FixedUpdate()
+    {
+        foreach (Collider c in Colliders)
+        {
+            Rigidbody rb = c.attachedRigidbody;
+            if (rb != null && rb != mainRb)
+            {
+                Vector3 offset = transform.position - c.transform.position;
+                Vector3 dir = offset / offset.sqrMagnitude * mainRb.mass * 2.0f;
+                Vector3 perpdir = Quaternion.AngleAxis(-90, Vector3.down) * Vector3.Normalize(dir) * 8.0f;
+
+                rb.AddForce(dir);
+                //rb.AddForce(perpdir);
+
+                Debug.DrawLine(c.transform.position, c.transform.position + dir * 0.1f);
+            }
+        }
+    }
+
+    public void RefreshColliders()
+    {
+        // note: inefficient. lock this collection on this call at some point + do not update every fixedupdate
+        Colliders = Physics.OverlapSphere(transform.position, range);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
