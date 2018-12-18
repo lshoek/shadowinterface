6dc774ebed417006de94d72f339086e4e3182515
Jordy van Miltenburg Mac <jordyvanmiltenburg@gmail.com>
2018-12-18 23:06:13 +0100

    Spikes and Deadzone with triggers

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    [SerializeField] float DeadZoneRotSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.down * Time.deltaTime * DeadZoneRotSpeed);
+		
+	}
+}
+
